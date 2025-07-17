caa184278e86fb48ad208ea765e8ea7f36a6a1e8
Tilak46 <pateltilak1711@gmail.com>
2025-07-17 19:06:04 +0530

    v1 Completed

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { CitiesProvider } from "./Contexts/CitiesContext";
+import { AuthProvider } from "./Contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+// import HomePage from "./pages/HomePage";
+// import Pricing from "./pages/Pricing";
+// import Product from "./pages/Product";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+import "./index.css";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
+function App() {
+  return (
+    <div>
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Suspense fallback={<SpinnerFullPage />}>
+              <Routes>
+                <Route index element={<HomePage />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="product" element={<Product />} />
+                <Route path="login" element={<Login />} />
+
+                <Route
+                  path="app"
+                  element={
+                    <ProtectedRoute>
+                      <AppLayout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Navigate replace to="cities" />} />
+
+                  <Route path="cities" element={<CityList />} />
+                  <Route path="cities/:id" element={<City />} />
+                  <Route path="countries" element={<CountryList />} />
+                  <Route path="form" element={<Form />} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
